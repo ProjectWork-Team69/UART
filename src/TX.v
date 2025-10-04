2207bd399ec77f2c7e4603b9c142f01b94b379c5
R0h1th-1DD4E2 <rohith.010404@gmail.com>
2025-10-04 21:25:11 +0530

    TX Design Completed

--- src/TX.v
+++ src/TX.v
@@ -4,18 +4,22 @@ module TX(
     input wire          clk,
     input wire          baud_clk,
     input wire          rst_n,
+
     input wire          start,
+    input wire          tx_en,
+
     input wire          fifo_en,
     input wire          fifo_empty,
-    input wire [8:0]    tx_data,
+
+    input wire [8:0]    tx_data, // can vary from 5 to 9 bits
     input wire [1:0]    parity, 
     input wire [2:0]    data_bits,
     input wire          stop_bit,
-    input wire          tx_en,
 
     output reg          tx,
     output reg          tx_done,
     output reg          tx_idle,
+    output wire         no_data // this is to indicate no data to transmit (fifo empty and fifo_en is high
 
 );
     // States
@@ -26,18 +30,26 @@ module TX(
               STOP = 3'b100;
 
     // registers
-    reg [1:0]  state, next_state; // state register
-    reg [10:0] temp_frame; // stores 
-    reg [3:0]  no_bits;
-    reg []
+    reg [1:0] state, next_state; // state register
+    reg [8:0] temp_frame; // stores 
+    reg [3:0] no_bits;
+    reg       stop_bit_copy; // copy of stop_bit
+    reg [1:0] parity_copy; // copy of parity
+    reg       parity_bit; // calulated parity
 
-    // update no. of bits anf frame size, only gets updateed when TX is idle 
+    assign no_data = fifo_en & fifo_empty;
+
+    // update no. of bits, only gets updated when TX is idle 
     always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) 
+        if (!rst_n) begin
             no_bits <= 4'b1000;
-            frame_size <= 4'b1010; // 8 data bits + 1 start + 1 stop
-        else 
-            if (cur_state == IDLE) begin
+            stop_bit_copy <= 1'b0;
+            parity_copy <= 2'b00;
+            parity_bit <= 1'b0;
+        end else begin
+            if (state == IDLE) begin
+                stop_bit_copy <= stop_bit;
+                parity_copy <= parity;
                 case (data_bits)
                     3'b000 : no_bits <= 4'b0101;
                     3'b001 : no_bits <= 4'b0110;
@@ -46,12 +58,38 @@ module TX(
                     3'b100 : no_bits <= 4'b1001;
                     default: no_bits <= 4'b1000;
                 endcase
-                frame_size <= no_bits + 4'b0001 + (^parity) + 4'b0001 + stop_bit; // Updated frame size
+            end
+            // why not in IDLE? can cause metastable state where no_bits is changing 
+            if (state == DATA_BIT) begin
+                // calculating parity
+                case(no_bits)
+                    4'd5: parity_bit <= ^tx_data[4:0];
+                    4'd6: parity_bit <= ^tx_data[5:0];
+                    4'd7: parity_bit <= ^tx_data[6:0];
+                    4'd8: parity_bit <= ^tx_data[7:0];
+                    4'd9: parity_bit <= ^tx_data[8:0];
+                    default: parity_bit <= ^tx_data[7:0];
+                endcase
+            end
+            // to cause 2 stop bits, stop_bit_copy is negated if it's found 1
+            if (state == STOP) begin
+                stop_bit_copy <= (stop_bit_copy) ? 1'b0 : stop_bit_copy;
             end
             else begin
                 no_bits <= no_bits;
-                frame_size <= frame_size;
+                stop_bit_copy <= stop_bit_copy;
+                parity_copy <= parity_copy;
+                parity_bit <= 1'b0;
             end
+        end
+    end
+
+    // take a copy of input data to transmit only when idle
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) 
+            temp_frame <= 9'b0;
+        else 
+            temp_frame <= (cur_state == IDLE) ? tx_data : temp_frame;
     end
 
     // state FSM logic
@@ -66,16 +104,79 @@ module TX(
     always @(*) begin
               case (state)
             IDLE:      
-                next_state = (start && tx_en) ? START : IDLE;
+                next_state = ((start && tx_en) || no_data) ? START : IDLE;
             START:     
-                next_state = (baud_tx) ? DATA_BITS : START;
-            DATA_BITS: 
-                next_state = (baud_tx && bit_index == 7) ? STOP : DATA_BITS;
-            STOP:      
-                next_state = (baud_tx) ? IDLE : STOP;
+                next_state = DATA_BITS;
+            DATA_BITS: begin
+                if (bit_index == no_bits - 1)
+                    next_state = (^parity_copy) ? PARITY : STOP;
+                else
+                    next_state = DATA_BITS;
+            end
+            PARITY:
+                next_state = STOP;
+            STOP: begin
+                // taking care of 1 or 2 stop bits
+                // if stop_bit is 0 then go to stop and 
+                // if stop_bit is 1 then stay for one more cycle (2 cycles in total)
+                if (stop_bit_copy) begin
+                    next_state = STOP;
+                    stop_bit_copy = 1'b0; // reset the copy
+                end else begin
+                    next_state = IDLE;
+                end
+            end
             default:   
                 next_state = IDLE;
         endcase
     end
 
+    // Sequential logic for state behavior
+    always @(posedge baud_clk or negedge rst_n) begin
+        if (!rst_n) begin
+            tx <= 1'b1;
+            tx_done <= 0;
+            tx_idle <= 1;
+        end 
+        else begin
+            case (state)
+                IDLE: begin
+                   tx <= 1'b1;
+                   tx_done <= 0;
+                   tx_idle <= 1;
+                end
+                START: begin
+                   tx <= 1'b0;
+                   tx_done <= 0;
+                   tx_idle <= 0;
+                end 
+                DATA_BITS: begin
+                    tx <= temp_frame[0];
+                    temp_frame <= {1'b0, temp_frame[8:1]}; // shift right
+                    tx_done <= 0;
+                    tx_idle <= 0;
+                end
+                PARITY: begin
+                    case(parity_copy)
+                        2'b01: tx <= parity_bit;      // odd parity
+                        2'b10: tx <= ~parity_bit;     // even parity
+                        default: tx <= 1'b0;
+                    endcase
+                    tx_done <= 0;
+                    tx_idle <= 0;
+                end
+                STOP: begin
+                    tx <= 1'b1;
+                    tx_done <= 1;
+                    tx_idle <= 0;
+                end
+                default : begin
+                    tx <= 1'b1;
+                    tx_done <= 0;
+                    tx_idle <= 1;
+                end
+            endcase
+       
+        end
+    end
 endmodule
